f6870b53c2c1cede2c35d856179ac5161cdc2443
Dikshya Pradhan <dikshyakasaju@gmail.com>
2023-08-09 11:42:36 +0530

    first commit

--- new.py
+++ new.py
@@ -1,2 +1,3 @@
 print("Hello World")
-print("This is a Git Tutorial V3 to test going back to previous versions")
+print("This is a Git Tutorial V1 before deletion")
+print("I am making some changes to the V1 configuration")
